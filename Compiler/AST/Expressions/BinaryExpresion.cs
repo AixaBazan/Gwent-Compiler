2f757ae6a0d0f4b34b7c7c649a85838c35c8b73f
AixaBazan <167341359+AixaBazan@users.noreply.github.com>
2024-08-14 20:23:04 -0400

    efe

--- Compiler/AST/Expressions/BinaryExpresion.cs
+++
@@ -1,7 +0,0 @@
-//Aritmetic Expressions
-public abstract class BinaryExpression : Expression
-{
-    public Expression? Right { get; set; }
-    public Expression? Left { get; set; }
-    public BinaryExpression(CodeLocation location) : base(location){}
-}
